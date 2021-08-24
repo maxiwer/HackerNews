d75a07e73472f35ee6db02b16d138c6a957ce4df
maxiwer <fma1cr@gmail.com>
2021-08-24 21:05:29 +0600

    basic routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeadingComponent } from './heading/heading.component';
+import { MainComponent } from './main/main.component';
+import { NewComponent } from './new/new.component';
+import { ShowComponent } from './show/show.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { AskComponent } from './ask/ask.component';
+import { AppRouting } from './app-routing';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeadingComponent
+    HeadingComponent,
+    MainComponent,
+    NewComponent,
+    ShowComponent,
+    JobsComponent,
+    AskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    AppRouting
   ],
-  providers: [],
+  providers: [
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
