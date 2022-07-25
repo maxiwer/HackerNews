5a0e1835e8405738789b88f73aec254632b35302
maxiwer <fma1cr@gmail.com>
2022-07-25 11:06:26 +0600

    add author modal window

--- src/app/shared/api/main.api.ts
+++ src/app/shared/api/main.api.ts
@@ -1,45 +1,49 @@
-import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { Observable } from 'rxjs';
-import { SECONDARY_URLS } from '../vars/secondary-urls';
-import { map, mergeMap, tap, switchMap, flatMap, filter, retry } from 'rxjs/operators';
+import {CommonModule} from '@angular/common';
+import {HttpClient} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {Observable} from 'rxjs';
+import {SECONDARY_URLS} from '../vars/secondary-urls';
+import {environment} from "../../../environments/environment.prod";
 
 @NgModule({
-    imports: [
-        CommonModule
-    ]
+  imports: [
+    CommonModule
+  ]
 })
 export class MainApi {
-    private MAIN_URL = 'https://hacker-news.firebaseio.com/v0/';
-    private readonly prettyPrint = 'print=pretty';
+  private readonly prettyPrint = 'print=pretty';
 
-    constructor(
-        private http: HttpClient,
-    ) { }
+  constructor(
+    private http: HttpClient,
+  ) {
+  }
 
-    getTopStories(): Observable<any> {
-        return this.http.get(this.MAIN_URL + SECONDARY_URLS.top + this.prettyPrint);
-    }
+  getTopStories(): Observable<any> {
+    return this.http.get(environment.MAIN_URL + SECONDARY_URLS.top + this.prettyPrint);
+  }
 
-    getNewStories(): Observable<any> {
-        return this.http.get(this.MAIN_URL + SECONDARY_URLS.new + this.prettyPrint);
-    }
-    
-    getAsks(): Observable<any> {
-        return this.http.get(this.MAIN_URL + SECONDARY_URLS.ask + this.prettyPrint);
-    }
+  getNewStories(): Observable<any> {
+    return this.http.get(environment.MAIN_URL + SECONDARY_URLS.new + this.prettyPrint);
+  }
 
-    getShows(): Observable<any> {
-        return this.http.get(this.MAIN_URL + SECONDARY_URLS.show + this.prettyPrint);
-    }
+  getAsks(): Observable<any> {
+    return this.http.get(environment.MAIN_URL + SECONDARY_URLS.ask + this.prettyPrint);
+  }
 
-    getJobs(): Observable<any> {
-        return this.http.get(this.MAIN_URL + SECONDARY_URLS.jobs + this.prettyPrint);
-    }
+  getShows(): Observable<any> {
+    return this.http.get(environment.MAIN_URL + SECONDARY_URLS.show + this.prettyPrint);
+  }
 
-    getStory(id: string): Observable<any> {
-        return this.http.get(this.MAIN_URL + `item/${id}.json?`)
-            // .pipe(map(data => JSON.stringify(data)));
-    }
+  getJobs(): Observable<any> {
+    return this.http.get(environment.MAIN_URL + SECONDARY_URLS.jobs + this.prettyPrint);
+  }
+
+  getStory(id: string): Observable<any> {
+    return this.http.get(environment.MAIN_URL + `item/${id}.json?`)
+    // .pipe(map(data => JSON.stringify(data)));
+  }
+
+  getAuthorInfo(author: string): Observable<any> {
+    return this.http.get(environment.MAIN_URL + SECONDARY_URLS.user + author + '.json');
+  }
 }
